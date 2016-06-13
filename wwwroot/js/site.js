fbb928c9d06dbdcef8aa453fa7d2ce501df7d6a4
nhatlm1@fsoft.com.vn <nhatlm1@fsoft.com.vn>
2016-06-13 11:35:18 +0700

    Add orginal source

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Write your Javascript code.
+angular.module('psBlogApp', [])
+.run(function () {
+    console.log('Hello, world, from psBlogApp!');
+})
+.directive('psCurrentTime', function () {
+    return {
+        template: '<h2 class="text-center">{{vm.currentTime}}</div>',
+        controllerAs: 'vm',
+        controller: function () {
+            var vm = this;
+            vm.currentTime = new Date().toLocaleTimeString();
+        }
+}
+});
